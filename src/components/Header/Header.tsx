307836602466e74022410f227270a94008be3fc4
NazmulAlamShawon <shawonkst15@gmail.com>
2023-12-20 15:43:43 +0600

    add new gotel link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link'
+
 const Header = () => {
   return (
-    <header className='py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between '>
-      
+    <header className='py-10 px-4 container mx auto text-xl flex flex-wrap md:flex-nowrap'>
+        <div className='flex items-center w-full md:h-2/3'>
+          <Link href='/' className="font-black text-[#F27405]">Hotelmania</Link>
+        </div>
+        <ul>
+
+        </ul>
+  
     </header>
   )
 }
