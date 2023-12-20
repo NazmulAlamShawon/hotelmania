24ef5186a25246c1380893eacf0347ed8def058f
NazmulAlamShawon <shawonkst15@gmail.com>
2023-12-20 21:32:39 +0600

    add new navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
-import Link from 'next/link';
-import {FaUserCircle} from 'react-icons/fa';
+import Link from 'next/link'
+ 
+
+
 
 
 const Header = () => {
@@ -8,12 +10,25 @@ const Header = () => {
         <div className='flex items-center w-full md:h-2/3'>
           <Link href='/' className="font-black text-[#F27405]">Hotelmania</Link>
         </div>
-        <ul className='flex items-center ml-5'>
-               <li>
-                  <Link href='/auth' className="cursorpointer"> 
-                   <FaUserCircle />
-                  </Link>
+        <ul className='flex items-center '>
+               <li className='flex items-center'>
+               <Link href="/" className="mr-5 text-[#F27405]">Home</Link>
                </li>
+              <li>
+              <Link href="/about" className="text-[#F27405] mr-5"> Menu</Link>
+              </li>
+              <li>
+              <Link href="/about" className="text-[#F27405] mr-5">Order</Link>
+              </li>
+              <li>
+              <Link href="/about" className="text-[#F27405] mr-5">Service</Link>
+              </li>
+              <li>
+              <Link href="/about" className="text-[#F27405] mr-5">Login</Link>
+              </li>
+              <li>
+              <Link href="/about" className="text-[#F27405]  ">Contact</Link>
+              </li>
         </ul>
   
     </header>
